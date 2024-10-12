26cf562546dd2914358a87e6d1a5ea3a80caa0e9
umakant3525 <umakant3525@gmail.com>
2024-10-12 12:40:19 +0530

    learn about own server creation in ts

---
+++ 03purets/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("HEllo ts ");
+let data1 = [1, 2, 3, 4];
+console.log(data1);
+console.table(data1);
---
+++ 03purets/index.ts
@@ -0,0 +1,6 @@
+console.log("HEllo ts ")
+
+let data1 : number[] = [1,2,3,4] ;
+
+console.log(data1)
+console.table(data1)
